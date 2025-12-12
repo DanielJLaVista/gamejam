708ce7e2700601ad49bfe28a5fe3eff518286dd9
Daniel La Vista <daniel.j.lavista@gmail.com>
2025-12-12 06:12:24 -0800

    Add monogame tutorial progress

---
+++ sandboxDan/monogameTutorial/DungeonSlime/Game1.cs
@@ -0,0 +1,166 @@
+﻿using System.ComponentModel;
+using System.Runtime.Serialization.Formatters;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
+using MonoGameLibrary.Graphics;
+
+namespace DungeonSlime;
+
+public class Game1 : Core {
+    //defines the slime animated sprite
+    private AnimatedSprite _slime;
+
+    //defines the bat animated sprite
+    private AnimatedSprite _bat;
+
+    //Tracks the position of the slime.
+    private Vector2 _slimePosition;
+
+    //Speed multiplier
+    private const float MOVEMENT_SPEED = 5.0f;
+
+    public Game1() : base ("Dungeon Slime", 1280, 720, false) {
+
+    }
+
+    protected override void Initialize() {
+        // TODO: Add your initialization logic here
+
+        base.Initialize ();
+    }
+
+    protected override void LoadContent() {
+        //Load the atlas texture using the content manager
+        Texture2D atlasTexture = Content.Load<Texture2D> ("images/atlas");
+
+        //Create the texture atlas from the XML configuration file
+        TextureAtlas atlas = TextureAtlas.FromFile (Content, "images/atlas-definition.xml");
+
+        atlas.PrintAtlas ();
+
+        //create the slime animated sprite from the atlas.
+        _slime = atlas.CreateAnimatedSprite ("slime-animation");
+        _slime.Scale = new Vector2 (4.0f, 4.0f);
+
+        //create the bat animated sprite from the atlas.
+        _bat = atlas.CreateAnimatedSprite ("bat-animation");
+        _bat.Scale = new Vector2 (4.0f, 4.0f);
+
+        base.LoadContent ();
+    }
+
+    protected override void Update(GameTime gameTime) {
+        if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState ().IsKeyDown (Keys.Escape))
+            Exit ();
+
+        // TODO: Add your update logic here
+        _slime.Update (gameTime);
+        _bat.Update (gameTime);
+
+        // Check for keyboard input and handle it.
+        CheckKeyboardInput ();
+
+        // Check for gamepad input and handle it.
+        CheckGamePadInput ();
+
+        base.Update (gameTime);
+    }
+
+    private void CheckKeyboardInput() {
+        //Get the state of keyboard input.
+        KeyboardState keyboardState = Keyboard.GetState ();
+
+        //If the space key is held down, the movement speed increases by 1.5
+        float speed = MOVEMENT_SPEED;
+        if (keyboardState.IsKeyDown (Keys.Space)) {
+            speed *= 1.5f;
+        }
+
+        //If the W or Up key is pressed, move the slime up on the screen.
+        if (keyboardState.IsKeyDown (Keys.W) || keyboardState.IsKeyDown (Keys.Up)) {
+            _slimePosition.Y -= speed;
+        }
+        //If the S or Down key is pressed, move the slime down on the screen.
+        if (keyboardState.IsKeyDown (Keys.S) || keyboardState.IsKeyDown (Keys.Down)) {
+            _slimePosition.Y += speed;
+        }
+
+        //If the A or Left key is pressed, move the slime left on the screen.
+        if (keyboardState.IsKeyDown (Keys.A) || keyboardState.IsKeyDown (Keys.Left)) {
+            _slimePosition.X -= speed;
+        }
+        //If the D or Right key is pressed, move the slime right on the screen.
+        if (keyboardState.IsKeyDown (Keys.D) || keyboardState.IsKeyDown (Keys.Right)) {
+            _slimePosition.X += speed;
+        }
+    }
+
+    private void CheckGamePadInput() {
+        GamePadState gamePadState = GamePad.GetState (PlayerIndex.One);
+
+        // If the A button is held down, the movement speed increases by 1.5
+        // and the gamepad vibrates as feedback to the player.
+        float speed = MOVEMENT_SPEED;
+        if (gamePadState.IsButtonDown (Buttons.A)) {
+            speed *= 1.5f;
+            GamePad.SetVibration (PlayerIndex.One, 1.0f, 1.0f);
+        } else {
+            GamePad.SetVibration (PlayerIndex.One, 0.0f, 0.0f);
+        }
+
+        // Check thumbstick first since it has priority over which gamepad input
+        // is movement.  It has priority since the thumbstick values provide a
+        // more granular analog value that can be used for movement.
+        if (gamePadState.ThumbSticks.Left != Vector2.Zero) {
+            _slimePosition.X += gamePadState.ThumbSticks.Left.X * speed;
+            _slimePosition.Y -= gamePadState.ThumbSticks.Left.Y * speed;
+        } else {
+            // If DPadUp is down, move the slime up on the screen.
+            if (gamePadState.IsButtonDown (Buttons.DPadUp)) {
+                _slimePosition.Y -= speed;
+            }
+
+            // If DPadDown is down, move the slime down on the screen.
+            if (gamePadState.IsButtonDown (Buttons.DPadDown)) {
+                _slimePosition.Y += speed;
+            }
+
+            // If DPapLeft is down, move the slime left on the screen.
+            if (gamePadState.IsButtonDown (Buttons.DPadLeft)) {
+                _slimePosition.X -= speed;
+            }
+
+            // If DPadRight is down, move the slime right on the screen.
+            if (gamePadState.IsButtonDown (Buttons.DPadRight)) {
+                _slimePosition.X += speed;
+            }
+        }
+    }
+
+    protected override void Draw(GameTime gameTime) {
+        GraphicsDevice.Clear (Color.CornflowerBlue);
+
+
+        // The bounds of the icon within the texture.
+        Rectangle iconSourceRect = new Rectangle (0, 0, 128, 128);
+
+        // The bounds of the word mark within the texture.
+        Rectangle wordmarkSourceRect = new Rectangle (150, 34, 458, 58);
+
+        //Begin the sprite batch to prepare for rendering.
+        SpriteBatch.Begin (samplerState: SamplerState.PointClamp);
+
+        // Draw the slime texture region at a scale of 4.0
+        _slime.Draw (SpriteBatch, _slimePosition);
+
+        // Draw the bat texture region 10px to the right of the slime at a scale of 4.0
+
+        _bat.Draw (SpriteBatch, new Vector2 (_slime.Width + 10, 0));
+        //Always end the sprite batch when finished
+        SpriteBatch.End ();
+
+        base.Draw (gameTime);
+    }
+}
---
+++ sandboxDan/monogameTutorial/MonoGameLibrary/Input /MouseButton.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonoGameLibrary.Input {
+    public enum MouseButton {
+        Left,
+        Middle,
+        Right,
+        XButton1,
+        XButton2
+    }
+}
